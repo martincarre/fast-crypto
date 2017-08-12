19c925cd6e10d023c24955125d4e106084f227d5
Ardzii <ardziig@gmail.com>
2017-08-12 21:52:38 +0200

    Added the full support for bitfinex and mongoose handler to mogodb

--- app.js
+++ app.js
@@ -1,28 +1,23 @@
 var rq = require('request-promise');
 
-function apiRequest(req) {
-  return rq({
-    url: `https://api.bitfinex.com/v1/pubticker/${req}`,
-    json: true,
-    resolveWithFullResponse: true
-  }).then((res) => {
-    var result = {};
-    Object.keys(res.body).forEach((k) => {
-      result= {
-            name: req,
-            a: res.body.ask,
-            b: res.body.bid,
-            m: res.body.mid,
-            c: res.body.last_price,
-            v: res.body.volume,
-            l: res.body.low,
-            h: res.body.high,
-            n: res.body.timestamp,
-      }
-    });
-    return result;
+function apiRequest(params, req) {
+  if (req) {
+    var options = {
+      uri: `https://api.bitfinex.com/v1/${params}/${req}`,
+      json: true,
+      resolveWithFullResponse: true
+    }
+  } else {
+    var options = {
+      uri: `https://api.bitfinex.com/v1/${params}`,
+      json: true,
+      resolveWithFullResponse: true
+    }
+  }
+  return rq(options).then((res) => {
+    return res;
   }).catch((err) => {
-    return err.statusCode;
+    return err;
   });
 };
 
--- config.js
+++ config.js
@@ -1,18 +1,87 @@
 var fs = require('fs');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+  mongoose.Promise = global.Promise;
+
 var apiRequest = require('./app.js').apiRequest;
 
+// ******************** MONGOOSE SERVER CONNECTION HANDLER:
+
+var server = mongoose.connect('mongodb://localhost/bitfinexAPICollection', {
+  useMongoClient: true,
+});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("CONNECTED!");
+});
+// ******************** MONGOOSE SCHEMA AND MODEL CONFIG:
+
+var tickerSchema = mongoose.Schema({
+  name: String,
+  a: Number,
+  b: Number,
+  m: Number,
+  c: Number,
+  v: Number,
+  l: Number,
+  h: Number,
+  n: Number
+});
+
+var Ticker = mongoose.model('Ticker', tickerSchema);
+
+// ******************** APP:
+
+  // ====== GET CRAWL LIST:
+function getList() {
+  var tickerArr = [];
+  return apiRequest('symbols').then((data) => {
+    var list = data.body
+    list.map((content) => {
+      if (content.indexOf('usd') !== -1 ) tickerArr.push(content);
+    });
+    return tickerArr;
+  });
+};
+
+  // ====== GET TICKER DATA:
 function config(list) {
-  return Promise.all(list.map(apiRequest))
+  return Promise.all(list.map(single))
   .then((res) => {
     return res;
-    // console.log(JSON.stringify(res, undefined, 3));
+  });
+};
+
+  // ===== GET SINGLE TICKER AND ADAPT TO LAYOUT HANDLER:
+
+function single(item) {
+  return apiRequest('pubticker', item).then((res) => {
+      var result = {};
+      Object.keys(res.body).forEach((k) => {
+        result= {
+              name: item,
+              a: res.body.ask,
+              b: res.body.bid,
+              m: res.body.mid,
+              c: res.body.last_price,
+              v: res.body.volume,
+              l: res.body.low,
+              h: res.body.high,
+              n: res.body.timestamp,
+        }
+      });
+      return result;
   })
 };
 
-function loop() {
+async function loop() {
+  var array = await getList();
+  var lengthArr = array.length
   setTimeout(
     async function () {
-      var list = ['btcusd', 'ltcusd', 'zecusd', 'etcusd'];
+      var list = array;
       var data = await config(list);
         if (data.indexOf(429) > -1) {
           console.log('[ERROR]: Too many requests. Waiting until next set of request...');
@@ -20,10 +89,26 @@ function loop() {
             loop();
           }, 60*1000);
         } else {
-          console.log(JSON.stringify(data, undefined, 3));
+          data.forEach((object) => {
+            var tick = new Ticker({
+              name: object.name,
+              a: object.a,
+              b: object.b,
+              m: object.m,
+              c: object.c,
+              v: object.v,
+              l: object.l,
+              h: object.h,
+              n: object.n,
+            });
+            tick.save(function(err, tick) {
+              if (err) return console.log(err);
+              console.log(`[SUCCESS]: ${tick.name} added to db!`);
+            });
+          });
           loop();
         }
-  }, 4000);
+  }, lengthArr*1000); // ************  LENGTHARR IS THERE TO AVOID 429
 };
 
 loop();
