35e3d0a5bd7945e741c214a37b89a82833bc0967
Ardzii <ardziig@gmail.com>
2017-10-09 12:02:58 +0200

    Trying to get the latest record from the db but not working for some reason - Parameter was changed from -1 to 1 for testing

--- compare.js
+++ compare.js
@@ -9,47 +9,41 @@ const { Krakentick } = require('./kraken/model/krakenModel');
 const { Bitstamptick } = require('./bitstamp/model/bitstampModel');
 var now = Math.floor(new Date());
 
+function bquery() {
+  return new Promise((resolve, reject) => {
+    Bitstamptick.findOne({}, {}, { sort: { created_at: 1 } }, function(
+      err,
+      tick
+    ) {
+      resolve(tick);
+    });
+  });
+}
+
 function kquery() {
   return new Promise((resolve, reject) => {
-    Krakentick.findOne(
-      {
-        iname: 'btcusd',
-        n: { $lt: now }
-      },
-      'mk c n',
-      function(err, tick) {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(tick);
-        }
-      }
-    );
+    Krakentick.findOne({}, {}, { sort: { created_at: 1 } }, function(
+      err,
+      tick
+    ) {
+      resolve(tick);
+    });
   });
 }
 
-function bquery() {
-  Bitstamptick.findOne({}, {}, { sort: { created_at: -1 } }, function(
-    err,
-    tick
-  ) {
-    console.log(tick);
-  });
+async function compare() {
+  var ktick = await kquery();
+  var btick = await bquery();
+  var dif = {
+    difc: ktick.c - btick.c,
+    ditn: ktick.n - btick.n,
+    difa: ktick.a - btick.a,
+    difb: ktick.b - btick.b,
+    kn: ktick.n,
+    bn: btick.n
+  };
+
+  console.log(JSON.stringify(dif, null, 3));
 }
 
-bquery();
-
-// async function compare() {
-//   var ktick = await kquery();
-//   var btick = await bquery();
-//   var dif = {
-//     difc: ktick.c - btick.c,
-//     ditn: ktick.n - btick.n,
-//     kn: ktick.n,
-//     bn: btick.n
-//   };
-
-//   console.log(JSON.stringify(dif, null, 3));
-// }
-
-// compare();
+compare();
