eed2ebbe68578f58337a96f3f63a43d94c491482
Ardzii <ardziig@gmail.com>
2017-10-10 15:25:41 +0200

    Working on compare

--- compare.js
+++ compare.js
@@ -39,48 +39,81 @@ function itbquery() {
   });
 }
 
+function brexquery() {
+  return new Promise((resolve, reject) => {
+    Bittrextick.findOne({}, {}, { sort: { n: -1 } }, function(err, tick) {
+      resolve(tick);
+    });
+  });
+}
+
 setInterval(async function() {
   var pos = 0;
   var ktick = await kquery();
   var bstick = await bsquery();
   var itbtick = await itbquery();
+  var brextick = await brexquery();
+
   var difkbs = {
+    // NOTE: MUST EACH OF THE CATEGORIES MUST HAVE AS MANY COMPs AS THERE ARE TICKS
     iname: ktick.iname,
-    comp_akbitb: itbtick.b - ktick.a,
-    comp_absbitb: itbtick.b - bstick.a,
-    comp_aitbbk: ktick.b - itbtick.a,
-    comp_aitbbbs: bstick.b - itbtick.a,
-    comp_akbbs: bstick.b - ktick.a,
-    comp_absbk: ktick.b - bstick.a,
-    difc_kbs: ktick.c - bstick.c,
-    difn_kbs: ktick.n - bstick.n,
-    difa_kbs: ktick.a - bstick.a,
-    difb_kbs: ktick.b - bstick.b,
-    spread_kba: ktick.a - ktick.b,
-    spread_bsba: bstick.a - bstick.b,
-    baspread_kbs: ktick.a - bstick.a - (ktick.b - bstick.b),
+    // NOTE: brextick *****
+    brexask: {
+      comp_abrexbk: ktick.b - brextick.a * 100,
+      comp_abrexbbs: bstick.b - brextick.a * 100,
+      comp_abrexbitb: itbtick.b - brextick.a * 100,
+      sreadbrexba: brextick.b * 100 - brextick.a * 100
+    },
+    // NOTE: ktick *****
+    kask: {
+      comp_akbbs: bstick.b - ktick.a,
+      comp_akbitb: itbtick.b - ktick.a,
+      comp_akbbrex: brextick.b * 100 - ktick.a,
+      spreadkba: ktick.b - ktick.a
+    },
+    // NOTE: itbtick ****
+    itbask: {
+      comp_aitbbbrex: brextick.b * 100 - itbtick.a,
+      comp_aitbbk: ktick.b - itbtick.a,
+      comp_aitbbbs: bstick.b - itbtick.a,
+      spreaditb: itbtick.b - itbtick.a
+    },
+    // NOTE: bstick ****
+    bsask: {
+      comp_absbitb: itbtick.b - bstick.a,
+      comp_absbk: ktick.b - bstick.a,
+      comp_absbbrex: brextick.b * 100 - bstick.a,
+      spread_bsba: bstick.a - bstick.b
+    },
+    // NOTE: END OF COMPARING BID ASK
+
+    // NOTE: Request time compare:
     kn: ktick.n,
     bsn: bstick.n,
     itbn: itbtick.n,
+    brexn: brextick.n,
+
+    // NOTE: _id of each tick for reference:
     k_id: ktick._id,
     bs_id: bstick._id,
     itb_id: itbtick._id
   };
 
-  if (Math.abs(difkbs.difn) > 1000) {
-    console.log('More than a second between requests: NA');
-  } else {
-    console.log(JSON.stringify(difkbs, null, 3));
-    if (difkbs.compakbbs > 0) {
-      console.log(
-        `Buy ${ktick.mk} @${ktick.b / 100} and sell ${bstick.mk} @${bstick.a /
-          100}. Earned: ${difkbs.compakbbs / 100}`
-      );
-    } else if (difkbs.compabsbk > 0) {
-      console.log(
-        `Buy ${bstick.mk} @${bstick.b / 100} and sell ${ktick.mk} @${ktick.a /
-          100}. Earned: ${difkbs.compabsbk / 100}`
-      );
-    }
-  }
+  console.log(JSON.stringify(difkbs, null, 3));
+  // if (Math.abs(difkbs.difn) > 1000) {
+  //   console.log('More than a second between requests: NA');
+  // } else {
+  //   console.log(JSON.stringify(difkbs, null, 3));
+  //   if (difkbs.compakbbs > 0) {
+  //     console.log(
+  //       `Buy ${ktick.mk} @${ktick.b / 100} and sell ${bstick.mk} @${bstick.a /
+  //         100}. Earned: ${difkbs.compakbbs / 100}`
+  //     );
+  //   } else if (difkbs.compabsbk > 0) {
+  //     console.log(
+  //       `Buy ${bstick.mk} @${bstick.b / 100} and sell ${ktick.mk} @${ktick.a /
+  //         100}. Earned: ${difkbs.compabsbk / 100}`
+  //     );
+  //   }
+  // }
 }, 1100);
